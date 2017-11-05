73bad36adeb0c8bb676ed2b82d06f2773fadd9da
Piotr <piotr.szablewski@vp.pl>
2017-11-05 18:50:06 +0100

    basic for validation

--- src/components/post_new.js
+++ src/components/post_new.js
@@ -1,12 +1,56 @@
 import React, { Component } from 'react';
-
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
+    renderField(field){
+        return(
+          <div className="form-group">
+              <label>{field.label}</label>
+              <input
+                  className="form-control"
+                  type="text"
+                  { ...field.input  }
+              />
+          </div>
+        );
+    }
     render(){
         return(
-            <div>Posts New!</div>
+            <form>
+                <Field
+                    label="Title"
+                    name="title"
+                    component={this.renderField}
+                />
+                <Field
+                    label="Categories"
+                    name="tags"
+                    component={this.renderField}
+                />
+                <Field
+                    label="Post content"
+                    name="content"
+                    component={this.renderField}
+                />
+            </form>
         )
     }
 }
+function validate( values ) {
+    const errors = {};
+    if(!values.title){
+        errors.title = "Enter a title";
+    }
+    if(!values.tags){
+        errors.tags = "Enter a category";
+    }
+    if(!values.cotent){
+        errors.cotent = "Enter a content";
+    }
 
-export default PostsNew;
+    return errors
+}
+export default reduxForm({
+    validate,
+    form: 'PostsNewForm'
+})(PostsNew);
