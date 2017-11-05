e8fcdf9c278d5c99ec73eab964749c2c13d70612
Piotr <piotr.szablewski@vp.pl>
2017-11-05 17:30:48 +0100

    setting redux-form

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 export const FETCH_POSTS = 'fetch_posts';
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api/';
-const API_KEY = '?key=PIOTR666';
-export function fetchPosts(  ) {
+const API_KEY = '?key=PIOTR6667';
+export  function fetchPosts(  ) {
     const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
     return {
         type: FETCH_POSTS,
